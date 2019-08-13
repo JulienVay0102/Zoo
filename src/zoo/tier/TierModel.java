9feb2a724370c0cd8db5e3eef5f7d2c700462af8
Julien <julien.vay@googlemail.com>
2019-08-13 14:20:12 +0200

    TierDAO reingebollert

--- src/zoo/tier/TierModel.java
+++ src/zoo/tier/TierModel.java
@@ -1,5 +1,5 @@
 package zoo.tier;
-
+import zoo.tierart.*;
 public class TierModel {
 	public TierModel() {
 		// TODO Auto-generated constructor stub
@@ -10,6 +10,8 @@ public class TierModel {
 	private int alter;
 	private char gender;
 	private String date;
+	private TierartModel tierart;
+	//private PflegerModel pfleger;
 	
 	public int getId() {
 		return id;
