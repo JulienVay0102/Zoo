2f33274819d67b36bcad77aad925399eba5d9a0d
Konstantin Mende <konstantin.mende@datagroup.de>
2019-08-13 14:17:08 +0200

    Alle Datenbank Funktionen

--- src/zoo/tierart/Test.java
+++ src/zoo/tierart/Test.java
@@ -1,10 +1,14 @@
 package zoo.tierart;
 
+import java.util.ArrayList;
+
 public class Test {
 	
 	public static void main (String[] args) {
 		
 		TierartModel tierart;
+		
+		ArrayList<TierartModel> tierartList = new ArrayList<TierartModel>();
 		
 		TierartDao tierartDao = new TierartDao();
 		
@@ -12,8 +16,13 @@ public class Test {
 		
 		System.out.println(tierart.getGattung());
 		
-		tierartDao.updateTierart("Lemur", 12, "Afrika", 1.5f, "Bananen" );
-
+		tierartList = tierartDao.getAllTierart();
+		
+		for (int i = 0; i<tierartList.size(); i++) {
+			System.out.println(tierartList.get(i).getGattung());
+		}
+	
+		
 	}
 
 }
