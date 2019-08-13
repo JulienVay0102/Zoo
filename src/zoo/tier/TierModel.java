d21d310a179e2e87cce4751bc739e344934a2da5
Julien <julien.vay@googlemail.com>
2019-08-13 13:14:32 +0200

    Main erstellt, test muss noch raus, TierController und TierModel erstellt TierView.fxml erstellt

---
+++ src/zoo/tier/TierModel.java
@@ -0,0 +1,52 @@
+package zoo.tier;
+
+public class TierModel {
+	public TierModel() {
+		// TODO Auto-generated constructor stub
+	}
+	private int id;
+	private String name;
+	private int height;
+	private int alter;
+	private char gender;
+	private String date;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getAlter() {
+		return alter;
+	}
+	public void setAlter(int alter) {
+		this.alter = alter;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+
+}
