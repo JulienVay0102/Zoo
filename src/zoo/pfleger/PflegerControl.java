255fcdda9e62b4d9125d658994c5a689decc9e79
MaximilianPegasus <Predator1>
2019-08-20 13:30:46 +0200

    Rebase

--- src/zoo/pfleger/PflegerControl.java
+++ src/zoo/pfleger/PflegerControl.java
@@ -3,21 +3,28 @@ package zoo.pfleger;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import zoo.utils.Main;
 import javafx.fxml.FXML;
 
 public class PflegerControl {
 	
+    public Main main;
+
 	@FXML
 	Button btnTierAnlegen = new Button();
-	
-	
 
-	public void tierAnlegen() {
+    public void switchScene(String sceneName){
+        main.switchScene(sceneName);
+    }
+    public void setMain(Main main){
+        this.main = main;
+    }
+
+public void tierAnlegen() {
 		
-		
-	};
-	
-	
-	
-	
+}
+
+
+
+
 }
