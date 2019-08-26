e0b08538e0c8d0708f1fd76ec993f285c952ec2d
Konstantin Mende <konstantin.mende@datagroup.de>
2019-08-26 14:12:39 +0200

    Jetzt aber

--- src/zoo/tier/TierController.java
+++ src/zoo/tier/TierController.java
@@ -10,14 +10,17 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import zoo.pfleger.PflegerDao;
 import zoo.tierart.TierartDao;
+import zoo.utils.AbstractController;
 import zoo.utils.Main;
+import zoo.utils.Session;
 
-
-public class TierController {
+public class TierController extends AbstractController {
 	private Main main;
+
 	public TierController() {
-		// TODO Auto-generated constructor stub
+// TODO Auto-generated constructor stub
 	}
+
 	@FXML
 	private Button backBtn;
 	@FXML
@@ -40,37 +43,45 @@ public class TierController {
 	private TextField tfGender;
 	@FXML
 	private TextField tfDate;
-	
-	@FXML
-	private void load() {
-		
-	}
-	
-	@FXML
-	private void goBack() throws IOException 
-    {
-	     
-	  switchScene("pflegerview");
-		
-    }
-	@FXML
-	private void updateAnimal() 
-    {
-       TierDAO.updateTier(Integer.valueOf(tfTierID.getText()), tfName.getText(), Integer.valueOf(tfHoehe.getText()),
-    		   Integer.valueOf(tfTieralter.getText()), tfGender.getText(), tfDate.getText()
-    		   , TierartDao.getTierartModel(tfGattung.getText()), PflegerDao.getPflegerModel(Integer.valueOf(tfPflegerID.getText())));
-    }
-	@FXML
-	private void deleteAnimal() 
-    {
-        TierDAO.deleteTier(Integer.valueOf(tfTierID.getText()));
-    }
-	public void switchScene(String sceneName){
-        main.switchScene(sceneName);
-    }
 
-    public void setMain(Main main){
-        this.main = main;
-    }
+	@FXML
+	private void goBack() throws IOException {
+
+		switchScene("pflegerview");
+
+	}
+
+	@FXML
+	private void updateAnimal() {
+		TierDAO.updateTier(Integer.valueOf(tfTierID.getText()), tfName.getText(), 0,
+				Integer.valueOf(tfTieralter.getText()), tfGender.getText(), tfDate.getText(),
+				TierartDao.getTierartModel(tfGattung.getText()),
+				PflegerDao.getPflegerModel(Integer.valueOf(tfPflegerID.getText())));
+		switchScene("pflegerview");
+	}
+
+	@FXML
+	private void deleteAnimal() {
+		TierDAO.deleteTier(Integer.valueOf(tfTierID.getText()));
+		switchScene("pflegerview");
+	}
+
+	public void switchScene(String sceneName) {
+		main.switchScene(sceneName);
+	}
+
+	public void setMain(Main main) {
+		this.main = main;
+	}
+
+	public void onLoad() {
+		tfTierID.setText(Integer.toString(Session.getInstance().getTier().getTierID()));
+		tfName.setText(Session.getInstance().getTier().getName());
+		tfTieralter.setText(Integer.toString(Session.getInstance().getTier().getTierAlter()));
+		tfGattung.setText(Session.getInstance().getTier().getTierart().getGattung());
+		tfPflegerID.setText(Integer.toString(Session.getInstance().getTier().getPfleger().getPflegerID()));
+		tfGender.setText(Session.getInstance().getTier().getGender());
+		tfDate.setText(Session.getInstance().getTier().getDate());
+	}
 
 }
