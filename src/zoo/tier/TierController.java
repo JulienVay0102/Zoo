621013e0328e72314a117af3364990b8432140f0
Julien <julien.vay@googlemail.com>
2019-08-26 09:20:02 +0200

    TierView aktualisiert

--- src/zoo/tier/TierController.java
+++ src/zoo/tier/TierController.java
@@ -6,11 +6,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import zoo.pfleger.PflegerDao;
+import zoo.tierart.TierartDao;
+import zoo.utils.Main;
 
 
 public class TierController {
-
+	private Main main;
 	public TierController() {
 		// TODO Auto-generated constructor stub
 	}
@@ -20,35 +24,53 @@ public class TierController {
 	private Button deleteBtn;
 	@FXML
 	private Button updateBtn;
+	@FXML
+	private TextField tfTierID;
+	@FXML
+	private TextField tfName;
+	@FXML
+	private TextField tfHoehe;
+	@FXML
+	private TextField tfTieralter;
+	@FXML
+	private TextField tfGattung;
+	@FXML
+	private TextField tfPflegerID;
+	@FXML
+	private TextField tfGender;
+	@FXML
+	private TextField tfDate;
+	
+	@FXML
+	private void load() {
+		
+	}
 	
 	@FXML
 	private void goBack() throws IOException 
     {
-	        Parent root;
-	        try {
-	            root = FXMLLoader.load(getClass().getClassLoader().getResource("zoo/tier/test.fxml"));
-	            Stage stage = new Stage();
-	            stage.setTitle("My New Stage Title");
-	            stage.setScene(new Scene(root, 450, 450));
-	            stage.show();
-	        }
-	        catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    
-		
-		
+	     
+	  switchScene("pflegerview");
 		
     }
 	@FXML
 	private void updateAnimal() 
     {
-        //AnimalDAO benötigt
+       TierDAO.updateTier(Integer.valueOf(tfTierID.getText()), tfName.getText(), Integer.valueOf(tfHoehe.getText()),
+    		   Integer.valueOf(tfTieralter.getText()), tfGender.getText(), tfDate.getText()
+    		   , TierartDao.getTierartModel(tfGattung.getText()), PflegerDao.getPflegerModel(Integer.valueOf(tfPflegerID.getText())));
     }
 	@FXML
 	private void deleteAnimal() 
     {
-        //DAO benötigt
+        TierDAO.deleteTier(Integer.valueOf(tfTierID.getText()));
     }
-	
+	public void switchScene(String sceneName){
+        main.switchScene(sceneName);
+    }
+
+    public void setMain(Main main){
+        this.main = main;
+    }
+
 }
